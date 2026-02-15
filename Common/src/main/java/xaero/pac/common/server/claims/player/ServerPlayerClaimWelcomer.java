f58a6ef8276462fd8e836a35196e6c0293e6a056
MapleSugar365 <steady.i@foxmail.com>
2026-02-15 22:28:25 +0800

    Minor adjustments suggested.

--- Common/src/main/java/xaero/pac/common/server/claims/player/ServerPlayerClaimWelcomer.java
+++ Common/src/main/java/xaero/pac/common/server/claims/player/ServerPlayerClaimWelcomer.java
@@ -86,9 +86,9 @@ public class ServerPlayerClaimWelcomer {
 		}
 		subTitleText = subTitleText.withStyle(s -> s.withColor(isOwner ? ChatFormatting.DARK_GREEN : hasAccess ? ChatFormatting.GOLD : ChatFormatting.DARK_RED));
 
-		MutableComponent subTitle = Component.literal("□ ").withStyle(s -> s.withColor(claimColor));
+		MutableComponent subTitle = Component.literal("> ").withStyle(s -> s.withColor(claimColor));
 		subTitle.getSiblings().add(subTitleText);
-		subTitle.getSiblings().add(Component.literal(" □").withStyle(s -> s.withColor(claimColor)));
+		subTitle.getSiblings().add(Component.literal(" <").withStyle(s -> s.withColor(claimColor)));
 		ClientboundSetActionBarTextPacket packet = new ClientboundSetActionBarTextPacket(subTitle);
 		player.connection.send(packet);
 
